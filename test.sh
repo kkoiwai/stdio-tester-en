57a769782c59d4797108ef0625f82ba3c9ecec32
Kosuke Koiwai (aka.) 小岩井 航介 <kk@kocko.org>
2016-10-01 06:46:44 +0900

    Globalization
    Translated comments and output to English

--- test.sh
+++ test.sh
@@ -1,49 +1,48 @@
 #! /bin/sh
-## https://github.com/kkoiwai/stdio-tester
-## 標準入出力のテストケースをソースに埋め込んで１ファイルでテストまで行う便利なシェルスクリプト
-## 使い方は http://qiita.com/kkoiwai/items/d70c21f92eaca6bf9939
+## https://github.com/kkoiwai/stdio-tester-en
+## Make testing with stdio easier by including test cases in the source code
 
-## 引数が1つ以外の場合はカレントディレクトリの最新ファイルを参照する
+## Use latest file as source in the current directly if no arg is specified.
 src=$1
 if [ $# -ne 1 ]; then
   src=`ls -1t | grep -v test.sh | head -n 1` 
 fi
 
-## 実行コマンド(１行の前提)
+## Command to run the source (should be a single line)
 runcmd=`cat $src | grep -A1  "^-- COMMAND --" | tail -1`
 runcmd="$runcmd $src"
-## 入力データ開始位置
+## line no to start input
 inputbegin=`cat $src | grep -n  "^-- INPUT --" | sed -e 's/:.*//g' -e '2,$d'`
 inputbegin=$[$inputbegin+1]
-## 入力データ終了位置(開始からの行数)
+## line no to end input
 inputlineno=`cat $src | tail -n +${inputbegin} | grep -n  "^--" | sed -e 's/:.*//g' -e '2,$d'`
 inputlineno=$[$inputlineno-1]
 inputend=$[$inputbegin+$inputlineno-1]
 
-## 入力データ
+## extract input data from source
 inputdata=`cat $src | sed -n -e ${inputbegin},${inputend}p`
 
-## 想定出力データ開始位置
+## line to start expected output
 outputbegin=`cat $src | grep -n  "^-- OUTPUT --" | sed -e 's/:.*//g' -e '2,$d'`
 outputbegin=$[$outputbegin+1]
-## 想定出力データ終了位置(開始からの行数)
+## line to end expected output
 outputlineno=`cat $src | tail -n +${outputbegin} | grep -n  "^--" | sed -e 's/:.*//g' -e '2,$d'`
 outputlineno=$[$outputlineno-1]
 outputend=$[$outputbegin+$outputlineno-1]
 
-## 想定出力データ
+## extract expected output data from source
 outputdata=`cat $src | sed -n -e ${outputbegin},${outputend}p`
 
-## 実行結果
+## result after running the source
 result=`echo "$inputdata" | $runcmd`
 
-## 結果表示
-echo 実行コマンド : $runcmd
-echo "\n実行結果"
+## Show the result
+echo Command : $runcmd
+echo "\nResult :"
 echo "$result"
-echo "\n想定実行結果"
+echo "\nExpected :"
 echo "$outputdata"
-echo "\n比較結果"
+echo "\nDiff :"
 echo "$outputdata" | ( echo "$result" | diff /dev/fd/3 -) 3<&0 && echo "OK!"
 
-## 一致しなければ diff がステータス1で終了。
+## diff quits with status 1 if there is a gap
